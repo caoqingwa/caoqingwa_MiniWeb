6be5e080c91650f33bb2f5f3f6ffa4f799358913
caoqingwa <1251972432@qq.com>
2026-04-23 10:14:49 +0800

    successfully running at linux

--- caoqingwa_MiniWeb/include/threadpool.h
+++ caoqingwa_MiniWeb/include/threadpool.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#include <winsock2.h>
+#include <cstddef>
 #include <stdexcept>
 #include <thread>
 #include <queue>
@@ -7,25 +7,24 @@
 #include <functional>
 #include <mutex>
 #include <condition_variable>
-
+#include <utility>
 
 class ThreadPool {
 public:
     ThreadPool(size_t numThreads);
-    
 
     template<class T>
     void enqueue(T&& t);
+
     ~ThreadPool();
+
 private:
     std::vector<std::thread> workers;
     std::queue<std::function<void()>> tasks;
-
     std::mutex queueMutex;
     std::condition_variable condition;
     bool stop;
 };
-
 
 template<class T>
 void ThreadPool::enqueue(T&& t) {
